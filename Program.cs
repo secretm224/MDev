db8b2ca29acd0f7532345c740eb560e92b53d214
HoseokMoon <secretm224@gmail.com>
2021-03-13 20:18:02 +0900

    skil test C#

--- Program.cs
+++ Program.cs
@@ -2,14 +2,42 @@
 
 namespace Mdev
 {
+    public class Solution {
+        public int solution(string s) {
+            int answer = 0;
+            string sign = "+";
+            if(s.Length >=1 && s.Length <=5){
+               if(s.IndexOf("-")>-1 || s.IndexOf("+") > -1){
+                  sign = s[0].ToString();
+                  //Console.WriteLine(sign);  
+                  answer = Int32.Parse(s.Substring(1,s.Length-1));
+                  if(sign != ""){
+                     if(sign == "+"){
+                        answer = +answer;         
+                     }else if(sign == "-"){
+                         
+                         answer = -answer;
+                     }
+
+                  }
+               }
+            }
+
+            return answer;
+        }   
+    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("</br>");
-            Console.WriteLine("Test commit");
-            
+            Solution sol = new Solution();
+            string str = "+1234";
+            int result = sol.solution(str);  
+            Console.WriteLine(result);
+            string str2 = "-1234";
+            int result2 = sol.solution(str2);
+            Console.WriteLine(result2);
         }
     }
 }
